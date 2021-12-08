7d7a6900a8d693c2ee20617874cee42646155f2e
Sai Srujana Kamireddi <saisrujanakamireddi55@gmail.com>
2021-12-08 23:26:21 +0530

    Hi without main

---
+++ StatisKeyWord/src/HiWithoutMain.java
@@ -0,0 +1,10 @@
+public class HiWithoutMain {
+
+    public static void main(String[] args){
+        System.out.println("hello world!!");
+    }
+
+    static {
+        System.out.println("hello there !!");
+    }
+}
